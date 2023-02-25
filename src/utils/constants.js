f6fe82ef590739b1ca50e55150744305ab3753e0
Saptarshi <bhattacharyasaptarshi2001@gmail.com>
2023-02-25 16:26:36 +0530

    abis and address

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,7 +1,7 @@
 import PaymentSplitterABI from "./PaymentSplitter.json";
 import SkillupABI from "./Skillup.json";
 
-export const PaymentSplitterAddress = "0xcf58aCe039F6E6a7f6F409378B6031E2A061b48A";
+export const PaymentSplitterAddress = "0x3DCb4aF8afBfB34960Eb229208e841ee35856cf5";
 export const SkillupAddress = "0x5658F3E40Cd4F73964B712D9F77a4fc15AE66Cb0";
 
 export const PaymentABI = PaymentSplitterABI.abi;
